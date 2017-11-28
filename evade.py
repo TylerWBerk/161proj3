d2063d588598a28b2ff6e0eeb8427bceb95bb57f
Tyler Woodard <t.woodard@berkeley.edu>
2017-11-27 21:53:32 -0800

    q3 add 7

--- evade.py
+++ evade.py
@@ -5,7 +5,7 @@ import common
 
 if __name__ == '__main__':
     # www.miit.gov.cn                                                           
-    target = "128.32.42.199"
+    target = "172.217.5.196"
     msg = open("msg.txt").read()
     
     myip = None
